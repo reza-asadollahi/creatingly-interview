caacc7514c5ea396599519be63c9a1cfe684d773
Reza Asadi <seyedreza.asadollahi@gmail.com>
2024-08-01 02:53:29 +0330

    - initiate backend (Node.js and Express) and frontend (Angular) project

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const http = require('http')
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`backend server is running on port ${PORT}`)
+})

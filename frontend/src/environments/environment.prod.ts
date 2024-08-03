a4eda27b6bfc83a86b75047937c15975cf8d1219
Reza Asadi <seyedreza.asadollahi@gmail.com>
2024-08-03 15:24:33 +0330

    chore: setup environment variable in frontend and add socket io dependency

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  BASE_URL: "http://localhost:80",
+  BASE_URL_API: "http://localhost:80/api",
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  BASE_URL: "http://localhost:3000",
+  BASE_URL_API: "http://localhost:3000/api",
+};
